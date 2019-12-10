b969405e5cf09d7ea4471f0701bd898f2d38fb3b
LaszloNagy85 <n.laszlo.uk@gmail.com>
2019-12-10 10:04:25 +0100

    removed unused lines

--- src/main/java/com/codecool/quest/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/quest/logic/actors/Skeleton.java
@@ -1,9 +1,6 @@
 package com.codecool.quest.logic.actors;
 
 import com.codecool.quest.logic.Cell;
-import com.codecool.quest.logic.actors.Actor;
-
-import java.util.*;
 
 public class Skeleton extends Actor {
 
