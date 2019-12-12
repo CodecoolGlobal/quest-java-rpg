de01f4447161d56875c44602eacff03441b3935c
Anna Czinege <anna.czinege3@gmail.com>
2019-12-12 16:33:31 +0100

    Add Yeti to dropItem method

--- src/main/java/com/codecool/quest/Main.java
+++ src/main/java/com/codecool/quest/Main.java
@@ -335,6 +335,7 @@ public class Main extends Application {
         try {
             this.map.getSkeleton().monsterIsDead();
             this.map.getGhost().monsterIsDead();
+            this.map.getYeti().monsterIsDead();
         } catch (Exception ignored){
 
         }
